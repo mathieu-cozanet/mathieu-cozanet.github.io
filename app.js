4abe948109533ed679c3c515a89ecab2f58cb136
mathieu-cozanet <apogithub@gmail.com>
2024-02-25 16:51:39 +0100

    ajout curseur mais reste a modifier zindex

--- app.js
+++ app.js
@@ -1,19 +1,21 @@
+const menu = document.getElementById('menu');
+const btn = document.getElementById('btn');
 const circle = document.getElementById('circle');
 
 document.addEventListener('mousemove', (e) => {
     const height = circle.offsetHeight;
     const width = circle.offsetWidth;
 
-    if (e.target.tagName === 'A' ||
-        e.target.tagName === 'BUTTON' ||
-        e.target.parentNode.tagName === 'BUTTON') {
+    if (e.target.tagName === 'CONTAINER' ||
+        e.target.tagName === 'H1' ||
+        e.target.parentNode.tagName === 'H1') {
         circle.classList.add('big');
     } else {
         circle.classList.remove('big');
     }
-
+// ICI on met un delai sur le mouvement de la souris
     setTimeout(() => { 
         circle.style.left = `${e.pageX - width/2}px`;
         circle.style.top = `${e.pageY - height/2}px`;
-    }, 20);
+    }, 50);
 });
